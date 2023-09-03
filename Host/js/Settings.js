2d5a47239162f5c9d3fac81749c5b5667849c922
Hiroit0 <viniciuslummertzg@gmail.com>
2023-09-03 12:45:23 -0300

    dor

--- Host/js/Settings.js
+++ Host/js/Settings.js
@@ -1,3 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import {getDatabase, ref, get, set, child, update, remove} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+import {getAuth, createUserWithEmailAndPassword,setPersistence,browserSessionPersistence } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-sTNLSRresl1l8dEdHUap3MDnMa8olWg",
+  authDomain: "tcc-01-14792.firebaseapp.com",
+  databaseURL: "https://tcc-01-14792-default-rtdb.firebaseio.com",
+  projectId: "tcc-01-14792",
+  storageBucket: "tcc-01-14792.appspot.com",
+  messagingSenderId: "432967975257",
+  appId: "1:432967975257:web:890f1cf2373a70fa5d8c48"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth();
+
 document.addEventListener('DOMContentLoaded', function() {
     const editButton = document.getElementById('editButton');
     const saveButton = document.getElementById('saveButton');
@@ -7,6 +25,34 @@ document.addEventListener('DOMContentLoaded', function() {
     
     saveButtonContainer.style.display = 'none'; // Hide the "Enviar" button initially
     
+    const auth = getAuth(); // Obtém a instância de autenticação Firebase
+    // Verifica se o usuário está autenticado
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            console.log(user)
+
+            // O usuário está autenticado, agora você pode buscar os dados do usuário no banco de dados em tempo real
+            const userRef = ref(db, 'Usuarios/' + "ajmhdsv");
+            
+            // Obtenha os dados do usuário no banco de dados em tempo real
+            get(userRef).then((snapshot) => {
+                if (snapshot.exists()) {
+                    const userData = snapshot.val();
+                    
+                    // Exibe os dados do usuário nos campos de exibição
+                    paragraphs[0].textContent = userData.Name;
+                    paragraphs[1].textContent = userData.Email;
+                    
+                    // Exibe os dados do usuário nos campos de edição
+                    inputs[0].value = userData.Name;
+                    inputs[1].value = userData.Email;
+                }
+            }).catch((error) => {
+                console.error('Erro ao buscar dados do usuário:', error);
+            });
+        }
+    });
+    
     editButton.addEventListener('click', function() {
         document.body.classList.add('edit-mode');
         
@@ -14,19 +60,38 @@ document.addEventListener('DOMContentLoaded', function() {
         inputs.forEach(input => input.style.display = 'block');
         
         editButton.style.display = 'none';
-        saveButtonContainer.style.display = 'block'; // Show the "Enviar" button when in edit mode
+        saveButtonContainer.style.display = 'block'; // Mostra o botão "Enviar" quando no modo de edição
     });
     
     saveButton.addEventListener('click', function() {
         document.body.classList.remove('edit-mode');
         
-        paragraphs.forEach((p, i) => {
-            p.textContent = inputs[i].value;
-            p.style.display = 'block';
-        });
-        inputs.forEach(input => input.style.display = 'none');
+        // Atualize os dados do usuário no banco de dados em tempo real
+        const name = inputs[0].value;
+        const email = inputs[1].value;
+        const user = auth.currentUser; // Obtém o usuário autenticado
         
-        editButton.style.display = 'block';
-        saveButtonContainer.style.display = 'none'; // Hide the "Enviar" button when changes are saved
+        if (user) {
+            // Atualize os dados do usuário no banco de dados em tempo real
+            update(ref(db, 'Usuarios/' + "ajmhdsv"), {
+                Name: name,
+                Email: email
+            }).then(() => {
+                console.log('Dados do usuário atualizados com sucesso.');
+                
+                // Exibe os dados atualizados nos campos de exibição
+                paragraphs[0].textContent = name;
+                paragraphs[1].textContent = email;
+                
+                // Oculta os campos de edição
+                inputs.forEach(input => input.style.display = 'none');
+                
+                // Mostra o botão "Editar"
+                editButton.style.display = 'block';
+                saveButtonContainer.style.display = 'none'; // Oculta o botão "Enviar" quando as alterações são salvas
+            }).catch((error) => {
+                console.error('Erro ao atualizar dados do usuário:', error);
+            });
+        }
     });
 });
--- Host/js/scriptUsers.js
+++ Host/js/scriptUsers.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import {getDatabase, ref, get, set, child, update, remove} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
-import {getAuth} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js"
+import {getAuth, createUserWithEmailAndPassword,setPersistence,browserSessionPersistence } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js"
+
 const firebaseConfig = {
   apiKey: "AIzaSyC-sTNLSRresl1l8dEdHUap3MDnMa8olWg",
   authDomain: "tcc-01-14792.firebaseapp.com",
@@ -8,8 +9,7 @@ const firebaseConfig = {
   projectId: "tcc-01-14792",
   storageBucket: "tcc-01-14792.appspot.com",
   messagingSenderId: "432967975257",
-  appId: "1:432967975257:web:2c48a5e7df9e3d3d5d8c48",
-  databaseURL: "https://tcc-01-14792-default-rtdb.firebaseio.com",
+  appId: "1:432967975257:web:890f1cf2373a70fa5d8c48"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,7 +18,7 @@ console.log(app);
 
 const db = getDatabase();
 const auth = getAuth();
-var enterID = document.querySelector("#enterID");
+var enterEmail = document.querySelector("#enterEmail");
 var enterName = document.querySelector("#enterName");
 var enterTel = document.querySelector("#enterTel");
 var enterCNPJ = document.querySelector("#enterCNPJ");
@@ -27,10 +27,42 @@ var enterDescricao = document.querySelector("#enterDescricao");
 
 var insertBtn = document.querySelector("#insert");
 
+
+document.getElementById("insert").addEventListener("click", function() {
+  var email =  document.getElementById("enterEmail").value;
+  var password = document.getElementById("enterPassword").value;
+  //For new registration
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up successfully, but we want to prevent automatic sign-in
+    const user = userCredential.user;
+    console.log(user);
+    alert("Registration successfully!!");
+    
+    // Configure the authentication persistence to "none"
+    setPersistence(auth, browserSessionPersistence)
+      .then(() => {
+        // Now, the user won't be automatically signed in on page reloads
+        window.location.replace('Login.htm');
+      })
+      .catch((error) => {
+        console.error("Error setting authentication persistence:", error);
+      });
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(errorMessage);
+    console.error(errorCode);
+    alert(error);
+  });
+	  		  
+});
+
 function InsertData() {
-  set(ref(db, "People/" + enterID.value),{
+  set(ref(db, "Empresas/" + enterName.value),{
       Name: enterName.value,
-      ID: enterID.value,
+      Email: enterEmail.value,
       tel: enterTel.value,
       CNPJ: enterCNPJ.value,
       Endereco: enterEndereco.value,
